5bb432162895cc34bef546f4ce48283a355fa4c0
Annah Nyarangi <anyarangi@gmail.com>
2023-11-14 20:03:24 -0500

    made some corrections to show Skills list

--- js/index.js
+++ js/index.js
@@ -2,15 +2,18 @@ var today = new Date();
 var thisYear = today.getFullYear();
 var footer = document.querySelector('footer');
 var copyright = document.createElement('p');
-copyright.innerHTML = "Your Name &copy; " + thisYear;
+copyright.innerHTML = "Annah Nyarangi &copy; " + thisYear;
 footer.appendChild(copyright);
-var skills = [
+const skills = [
     "JavaScript",
     "HTML",
     "CSS",
 ];
-var skillsSection = document.querySelector('#skills');
-var skillsList = skillsSection.querySelector('ul');
-for (var i=0, i<skills.length i++) {
-    var currentSkill = skills[i];
+const skillsSection = document.querySelector('#skills');
+console.log(skillsSection);
+const skillsList = skillsSection.querySelector('ul');
+for (var i = 0; i < skills.length; i++) {
+    const skill =document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill); 
 } 
