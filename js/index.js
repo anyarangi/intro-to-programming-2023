13f11bc1d8f13893ab05ce9c9f53580534eae1a1
Annah Nyarangi <anyarangi@gmail.com>
2023-11-28 23:47:43 -0500

    lesson 4-3

--- js/index.js
+++ js/index.js
@@ -17,3 +17,30 @@ for (var i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill); 
 } 
+const messageForm = document.forms.leave_message;
+messageForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const userName = event.target.usersName.value;
+    const userEmail = event.target.usersEmail.value;
+    const userMessage = event.target.usersMessage.value;
+    console.log('User Name:', userName);
+    console.log('User Email:', userEmail);
+    console.log('User Message:', userMessage);
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    messageForm.reset();
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `
+        <a href="mailto:${userEmail}">${userName}</a>
+        <span>${userMessage}</span>
+    `;
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', function () {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+});
