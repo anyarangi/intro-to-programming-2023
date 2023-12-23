5ac8cfb4dc3a814fb4a160b847db627abb202fc0
Annah Nyarangi <anyarangi@gmail.com>
2023-12-23 10:43:34 -0500

    lesson-4-3 final corrections

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const skills = [
 ];
 const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
-for (let var i = 0; i < skills.length; i++) {
+for ( let i = 0; i < skills.length; i++) {
     const skill =document.createElement('li');
     skill.innerText = skills[i];
     skillsList.appendChild(skill); 
